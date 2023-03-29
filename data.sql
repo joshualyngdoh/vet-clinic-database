53bfd6673ecabb138d73acd73c7c1d49da7d70a0
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2023-03-29 17:21:45 +0530

    Changes after review

--- data.sql
+++ data.sql
@@ -1,13 +1,13 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts)
-VALUES ('Agumon', '2020-02-3', 'Yes', 10.23, 0);
+VALUES ('Agumon', '2020-02-3', 'True', 10.23, 0);
 
 INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts)
-VALUES ('Gabumon', '2018-11-15', 'Yes', 8, 2);
+VALUES ('Gabumon', '2018-11-15', 'True', 8, 2);
 
 INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts)
-VALUES ('Pikachu.', '2021-01-7', 'No', 15.04, 1);
+VALUES ('Pikachu.', '2021-01-7', 'False', 15.04, 1);
 
 INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts)
-VALUES ('Devimon', '2017-05-12', 'Yes', 11, 5);
+VALUES ('Devimon', '2017-05-12', 'True', 11, 5);
